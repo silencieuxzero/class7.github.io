42ce5721bbcb08c8a5213c0cc58bd56442665c43
Senhsucht <tech@sehnsucht.top>
2024-05-24 16:54:34 +0800

    fix: :bug: fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,6 @@ export async function GET(context) {
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            customData: post.data.customData,
             // 从 `slug` 属性计算出 RSS 链接
             // 这个例子假设所有的文章都被渲染为 `/blog/[slug]` 路由
             link: `/posts/${post.slug}/`,
